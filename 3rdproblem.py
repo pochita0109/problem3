940c287c2ce7ccaf462801d69796d682da1cf29a
pochita0109 <akosicosta@gmail.com>
2023-04-19 22:10:56 -0700

    first commit

---
+++ 3rdproblem.py
@@ -0,0 +1,31 @@
+# Kenneth John Costa
+# Assignment #2
+# The Vigenere Cipher
+
+print("VIGENERE CIPHER".center(45, "="))
+
+# Ask the user to input their name
+
+# Ask the user to input the message and keyword
+
+# Convert the message and keyword to uppercase
+
+# Convert the message and keyword with no spaces
+
+# Convert the keyword with in order to become same character with message
+
+# Converting the message and key into ciphertext
+
+#Convert the letters of message into numbers 
+
+# Convert the letters of keyword into numbers
+
+# Take the result mod 26 
+
+# Match the converted numbers into its equivalent letter
+
+# Save the ciphertext
+
+# Print the result
+
+# Ask the user if he/she wants to continue
